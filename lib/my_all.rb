555ec4b997f767af437eb4635732d38a9e41e7a5
Tharon Weighill <tharonweighill@gmail.com>
2020-08-02 20:25:34 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -5,15 +5,15 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i = i + 1
   end
- 
- 
- 
- 
- 
- 
- 
- 
- 
+ all_odd = [1,3].all? do |number|
+
+end #=> true
+all_odd #=> true
+
+[4, 8, 9, 984].any?{|i| i < 9842}
+
+[1,2,3].detect{|i| i.odd?} #=> 1
+
   if block_return_values.include?(false)
     false
   else
